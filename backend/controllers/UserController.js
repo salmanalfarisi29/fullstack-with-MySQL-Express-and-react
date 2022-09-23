7b500e91d5594b7164dc703346a79ce8dd9a8d1c
salmanalfarisi29 <salmannalfarisii994@gmail.com>
2022-09-24 02:15:34 +0700

    menghubungkan API menggunakan REST API

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -8,3 +8,16 @@ export const getUsers = async(req, res) =>{
         console.log(error.message);
     }
  }
+
+ export const getUserById = async(req, res) =>{
+    try {
+        const response = await User.findOne();({
+            where:{
+                id: req.params.id
+            }
+        });
+        res.status(200).json(response);
+    } catch (error) {
+        console.log(error.message);
+    }
+ }
--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -1,8 +1,12 @@
 import express from "express";
-import {getUsers} from "../controllers/UserController.js";
+import {
+    getUsers, 
+    getUserById
+} from "../controllers/UserController.js";
 
 const router = express.Router();
 
 router.get('/users', getUsers);
+router.get('/users/:id', getUserById);
 
 export default router;
