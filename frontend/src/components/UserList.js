63902ac9223679a4e2890d82c458befe59e2fe25
salmanalfarisi29 <salmannalfarisii994@gmail.com>
2022-09-26 10:29:58 +0700

    menambahkan table

---
+++ frontend/src/components/UserList.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const UserList = () => {
+  return (
+    <div className="columns mt-5 is-centered">
+        <div className="column is-half"></div>
+        <table>
+            <thead>
+                <tr>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                </tr>
+            </thead>
+            <body>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </body>
+        </table>
+    </div>
+  );
+};
+
+export default UserList;
